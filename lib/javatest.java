61d3a4d8d1c17b228f638b6e332e24637945fafe
TMsangohan <tomtommertens2@gmail.com>
2016-05-23 14:09:41 +0200

    adding jar files

---
+++ lib/javatest.java
@@ -0,0 +1,8 @@
+package accsoft.cals.extr.domain;
+
+
+public class javatest { 
+   public static void main(String[] args) { 
+      System.out.println(getLastCompletedLHCFillAndBeamModes(4444));
+   }
+}
